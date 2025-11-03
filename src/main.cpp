6a8651e38c6f0575bc9bb4eaf65b4a9e9462f32d
pppjula <160844030+pppjula@users.noreply.github.com>
2025-11-03 15:23:05 +0000

    ghg

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 //imie i nazwisko
 
 #include <iostream>
-int main() {
-    std::cout << "Hello, Matura 2025!\n";
+using namespace std;
+
+string z3_1_path_symbole_txt =
+}void zadanie3_1(){
+    ifstram plik(z3_1)
+}
+int main()  {
+   
     return 0;
 }
