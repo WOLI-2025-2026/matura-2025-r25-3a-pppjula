fdb9befa76645d1cc9c98ce61e5263f35565ac7e
pppjula <160844030+pppjula@users.noreply.github.com>
2025-11-11 11:46:41 +0000

    nowe

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,28 @@
-//imie i nazwisko
-
 #include <iostream>
+#include <fstream> 
 using namespace std;
-
-string z3_1_path_symbole_txt =
-}void zadanie3_1(){
-    ifstram plik(z3_1)
+string z2_1_path_symbole_txt * "/workspaces/matura-2025-r25-3a-Marianka321/zalaczniki-2025/symbole.txt";
+bool czy_palindrom(string tekst){
+    int ile_znakow = tekst.length();
+    for (int i =0;i<ile_znakow/2;i++){
+        if(tekst[i] != tekst[ile_znakow-i-1])
+        return false;
+    }
+    return true;
 }
-int main()  {
-   
-    return 0;
+void Zadanie2_1(){
+    ifstream plik(z2_1_path_symbole_txt);
+    if (!plik.is_open()){
+        cerr<<"Nie można otworzyć pliku"<<endl;
+    }
+    string linia;
+    while(plik>>linia){
+        if(czy_palindrom)
+        cout<<linia<<endl;
+    }
+
+    plik.close();
+}
+int main() {
+
 }
