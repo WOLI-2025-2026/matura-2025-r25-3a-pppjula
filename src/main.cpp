ce57ff415db953ce58b251ea90ee32302e7080ef
pppjula <160844030+pppjula@users.noreply.github.com>
2025-11-13 09:25:08 +0000

    zad2_1

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,49 @@
 #include <iostream>
-#include <fstream> 
+#include <fstream>
+
 using namespace std;
-string z2_1_path_symbole_txt * "/workspaces/matura-2025-r25-3a-Marianka321/zalaczniki-2025/symbole.txt";
-bool czy_palindrom(string tekst){
-    int ile_znakow = tekst.length();
-    for (int i =0;i<ile_znakow/2;i++){
-        if(tekst[i] != tekst[ile_znakow-i-1])
-        return false;
+
+string z3_1_path_symbole_txt = "../zalaczniki-2025/symbole.txt";
+
+bool czy_palindrom(string slowo)
+{
+    int dlugosc = slowo.length();
+    for(int i = 0; i < dlugosc / 2; i++)
+    {
+        if(slowo[i] != slowo[dlugosc - i - 1])
+        {
+            return false;
+        }
     }
     return true;
 }
-void Zadanie2_1(){
-    ifstream plik(z2_1_path_symbole_txt);
-    if (!plik.is_open()){
-        cerr<<"Nie można otworzyć pliku"<<endl;
+
+void zadanie2_1()
+{
+    ifstream plik(z3_1_path_symbole_txt);
+    if(!plik.is_open())
+    {
+        cerr << "nie mozna otworzyc pliku" << endl;
     }
+
     string linia;
-    while(plik>>linia){
-        if(czy_palindrom)
-        cout<<linia<<endl;
+    while(plik >> linia)
+    {
+        if (czy_palindrom(linia))
+        {
+            cout << linia << endl;
+        }
     }
+
 
     plik.close();
-}
-int main() {
 
+}
+
+
+int main()
+{
+
+    zadanie2_1();
+    return 0;
 }
